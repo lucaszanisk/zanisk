6d1e263672b9dab468c304a697d517beb6956966
lucaszanisk <lucaszanisk@gmail.com>
2022-08-24 16:44:20 -0300

    Adicionando feature EmailJs

--- src/components/Contato/index.tsx
+++ src/components/Contato/index.tsx
@@ -1,20 +1,30 @@
 import bandeiraVerde from 'public/svg/bandeira-3.svg';
-import { Form } from '@unform/web';
-import Input from '@/components/Input';
-import { useCallback, useRef } from 'react';
-import { sendEmail } from '@/services/contactMe';
-import { Scope } from '@unform/core';
 
+import { useRef } from 'react';
+
+import emailjs from '@emailjs/browser';
 export default function Contato() {
-  const formRef = useRef({});
+  const form = useRef('');
 
-  const handleSubmit = useCallback(async (data: object) => {
-    try {
-      await sendEmail(data);
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_g5nbejg',
+        'template_i2kd63q',
+        form.current,
+        '0SDdCrP2a-y1a9AEk'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
 
   return (
     <>
@@ -30,45 +40,43 @@ export default function Contato() {
           Vamos bater um papo? <br /> Deixa uma msg pra mim <br /> :D
         </h2>
       </div>
-
-      <Form
-        className='mx-auto mt-9 grid w-1/2 grid-rows-2'
-        onSubmit={(data) => {
-          handleSubmit(data);
-        }}
+      <form
+        className=' mx-auto  grid w-1/2 grid-cols-1 font-bold'
+        ref={form}
+        onSubmit={handleSubmit}
       >
-        <Scope path='template_params'>
-          <Input
-            className='mb-9 w-1/2 bg-zanisk-sand'
-            name='email'
-            ref={formRef}
-            placeholder='Digite seu email'
-            type='email'
-          />
-          <Input
-            className='mb-9 w-1/4 bg-zanisk-sand'
-            name='assunto'
-            ref={formRef}
-            placeholder='Sobre o que vamos conversar?'
-            type='text'
-          />
+        <input
+          placeholder='Digite seu nome'
+          className='mx-auto mb-4 w-1/2 rounded-full bg-zanisk-sand focus:opacity-90'
+          type='text'
+          name='from_name'
+        />
 
-          <Input
-            className='h-full bg-zanisk-sand'
-            name='mensagem'
-            ref={formRef}
-            placeholder='Digite sua mensagem'
-            type='text'
-          />
-        </Scope>
-        <button
-          className=' mx-auto my-20 h-10 w-1/5 rounded-full bg-zanisk-sand font-bold text-zanisk-green hover:opacity-50'
+        <input
+          placeholder='Sobre que assunto deseja conversar?'
+          className=' mx-auto mb-4 w-1/2 rounded-full bg-zanisk-sand focus:opacity-90'
+          type='text'
+          name='subject'
+        />
+
+        <input
+          placeholder='Digite seu email'
+          className=' mx-auto mb-4 w-1/2 rounded-full bg-zanisk-sand focus:opacity-90'
+          type='email'
+          name='reply_to'
+        />
+
+        <textarea
+          placeholder='Digite sua mensagem aqui'
+          className='mb-4 rounded-full bg-zanisk-sand pl-9 focus:opacity-90'
+          name='message'
+        />
+        <input
+          className='my-4 mx-auto w-1/5  rounded-full bg-zanisk-sand hover:cursor-pointer hover:opacity-70 '
           type='submit'
-        >
-          Enviar
-        </button>
-      </Form>
-      <div className='h-28 bg-zanisk-sand'></div>
+          value='Enviar'
+        />
+      </form>
     </>
   );
 }
