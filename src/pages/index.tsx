7e59a16e6308a5431b5eae61778139cb95cb0bbd
lucaszanisk <lucaszanisk@gmail.com>
2022-10-10 15:24:49 -0300

    retirando planeta da pagina principal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,17 @@
 import Banner from '@/components/layout/Banner';
 import Layout from '@/components/layout/Layout';
 import Seo from '@/components/Seo';
-import planet from 'public/svg/planet.svg';
-import Image from 'next/image';
+// import planet from 'public/svg/planet.svg';
+// import Image from 'next/image';
 
 export default function Home() {
   return (
     <Layout>
       <Seo />
       <Banner />
-      <span className='mx-auto mt-16 grid '>
+      {/* <span className='mx-auto mt-16 grid '>
         <Image className='animate-spin' src={planet} width={50} height={50} />
-      </span>
+      </span> */}
     </Layout>
   );
 }
